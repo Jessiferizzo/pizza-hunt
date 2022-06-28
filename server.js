592e2778390f5de5ccd5b3c7fc42ebd5b897356c
Jessica <jsisavath@gmail.com>
2022-06-27 23:17:55 -0500

    moved space back

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static('public'));
 
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/PIZZA-HUNT', {
-  useNewUrlParser: true,
+  useNewUrlParser:true,
   useUnifiedTopology: true
 });
 
